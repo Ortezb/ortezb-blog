e46018c7b7f4d1def2ef7780751db5f65037568f
Brandon Adonis Ortez <phoenixflower111@hotmail.com>
2015-02-09 15:02:12 -0800

    hi
    bye

---
+++ model/Database.php
@@ -0,0 +1,9 @@
+<?php
+class Database {
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+}
+
--- model/config.php
+++ model/config.php
@@ -1,2 +1,7 @@
 <?php
     $path = "/ortezb-blog/";
+    
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "blog_db";
