53f9f55778860f25a10599f6943d39161b678c47
Brandon Adonis Ortez <phoenixflower111@hotmail.com>
2015-02-10 15:02:37 -0800

    hi
    bye

--- model/Database.php
+++ model/Database.php
@@ -1,9 +1,60 @@
 <?php
+
 class Database {
+
     private $connection;
     private $host;
     private $username;
     private $password;
     private $database;
-}
 
+    public function __construct($host, $username, $password, $database) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+
+        $this->connection = new mysqli($host, $username, $password);
+
+        if ($connection->connect_error) {
+            die("<p>Error: " . $connection->connect_error . "</p>");
+        }
+
+        $exists = $connection->select_db($database);
+
+        if (!$exists) {
+            $query = $connection->query("CREATE DATABASE $database");
+
+            if ($query) {
+                echo "<p>Successfully created database: " . $database . "</p>";
+            }
+        } else {
+            echo "<p>Database already exists.</p>";
+        }
+    }
+
+    public function openConnection() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
+    }
+
+    public function closeConnection() {
+        if (isset($this->connection)) {
+            $this->connection->close();
+        }
+    }
+
+    public function query($string) {
+        $this->openConnection();
+
+        $query = $this->connection->query($string);
+
+        $this->closeConnection();
+
+        return $query;
+    }
+
+}
